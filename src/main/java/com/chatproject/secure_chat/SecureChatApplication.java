7f686582546035ae47e1006281212b11bae48655
강민제 <nolam@naver.com>
2025-08-19 21:16:17 +0900

    init: spring boot secure-chat

---
+++ src/main/java/com/chatproject/secure_chat/SecureChatApplication.java
@@ -0,0 +1,13 @@
+package com.chatproject.secure_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureChatApplication.class, args);
+	}
+
+}
