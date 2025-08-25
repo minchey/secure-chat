787e7db69d693f69aa09ba875dc9e01c04e617cb
강민제 <nolam@naver.com>
2025-08-25 21:48:31 +0900

    Outbound doc 수정

--- src/main/java/com/chatproject/secure_chat/transport/Outbound.java
+++ src/main/java/com/chatproject/secure_chat/transport/Outbound.java
@@ -2,10 +2,11 @@ package com.chatproject.secure_chat.transport;
 
 import com.chatproject.secure_chat.model.MsgFormat;
 
+
 /**
- * "메시지를 밖으로 보내는 방법"을 약속하는 간단한 인터페이스입니다.
- * - 지금은 콘솔 출력용 구현(ConsoleOutbound)을 다음 단계에서 만듭니다.
- * - 나중엔 소켓/웹소켓 전송 구현으로 쉽게 교체할 수 있게 합니다.
+ * "메시지를 밖으로 보내는 방법"을 약속하는 매우 간단한 인터페이스입니다.
+ * 구현체 예: 콘솔 출력(ConsoleOutbound), 소켓 전송(SocketOutbound) 등.
+ * 목적: ChatRoom이 전송 방식의 세부 구현에 묶이지 않도록(결합도↓).
  */
 public interface Outbound {
 
